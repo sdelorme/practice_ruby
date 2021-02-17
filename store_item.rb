1e8e0b6c5e04a082dd628e8370bd326125a693bf
Seth Delorme <delorme.s.m@gmail.com>
2021-02-16 18:56:38 -0600

    defines print_info method

--- store_item.rb
+++ store_item.rb
@@ -1,6 +1,6 @@
-hat1 = {"Type" => "snapback", "Color" => "red", "Cost" => 30}
-hat2 = {:Type => "winter", :Color => "blue", :Cost => 20}
-hat3 = {"Type" => "tophat", "Color" => "black", "Cost" => 50}
+# hat1 = {"Type" => "snapback", "Color" => "red", "Cost" => 30}
+# hat2 = {:Type => "winter", :Color => "blue", :Cost => 20}
+# hat3 = {"Type" => "tophat", "Color" => "black", "Cost" => 50}
 
 
 
@@ -8,12 +8,32 @@ hat3 = {"Type" => "tophat", "Color" => "black", "Cost" => 50}
 #a) Choose which attributes should have “reader” methods and which attributes should have “writer” methods.
 # b) Create an instance from your store item class. Use puts statements to print the 3 attributes individually to the terminal.
 
-class Store
+class Hat
 
-  def initialize("Type", "Color", "Cost")
+  def initialize(type, color, cost)
     @type = type
     @color = color
     @cost = cost
   end
 
+  def print_info
+    puts "This is a #{@type} hat. It is #{@color} and costs $#{@cost}"
+  end
+
+  def type
+  @type
+  end
+
+  def color
+  @color
+  end
+
+  def cost
+    @cost
+  end
+
+
 end
+
+store_item = Hat.new("snapback", "red", 30)
+
