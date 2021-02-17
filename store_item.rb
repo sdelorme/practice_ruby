ba1313c9a550c26407bf46f6d97d590a9d0436a4
Seth Delorme <delorme.s.m@gmail.com>
2021-02-16 19:12:10 -0600

    adds attribute accessor method to Hat class

--- store_item.rb
+++ store_item.rb
@@ -10,14 +10,18 @@
 
 class Hat
 
-  def initialize(type, color, cost)
+  attr_reader :size
+  attr_accessor :size
+
+  def initialize(type, color, cost, size)
     @type = type
     @color = color
     @cost = cost
+    @size = size
   end
 
   def print_info
-    puts "This is a #{@type} hat. It is #{@color} and costs $#{@cost}."
+    puts "This is a #{@type} hat. It is #{@color}, size #{@size}, and costs $#{@cost}."
   end
 
   def inflation
@@ -39,15 +43,18 @@ class Hat
 
 end
 
-store_item1 = Hat.new("snapback", "red", 30)
+store_item1 = Hat.new("snapback", "red", 30, "large")
 store_item1.print_info
 puts store_item1.type
 
-store_item2 = Hat.new("beanie", "blue", 25)
+store_item2 = Hat.new("beanie", "blue", 25, "small" )
 store_item2.print_info
 puts store_item2.cost
 store_item2.inflation
 puts store_item2.cost
+puts store_item2.size
+store_item2.size = "medium"
+puts store_item2.size
 
 
 
